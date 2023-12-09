9c774b06b853813e9b787674582e5eaf31f0f1fb
tuyenldhust <tuyen.ld194715@sis.hust.edu.vn>
2023-12-09 22:26:07 +0700

    fix not found staticfiles backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,8 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
