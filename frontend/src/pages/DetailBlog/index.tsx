1a57ff3711cc1bc171a12d1be871ec5adb4394cd
loc <nguyengialoc7@gmail.com>
2023-12-18 16:18:29 +0700

    Full UI detail Page

--- frontend/src/pages/DetailBlog/index.tsx
+++ frontend/src/pages/DetailBlog/index.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React from "react"
+import { useParams } from "react-router-dom";
+import { ClockCircleOutlined } from "@ant-design/icons";
+import likeButton from "../../assets/images/LikeButton.png"
 
 const DetailBlog = () => {
+    const { id } = useParams();
+
     return (
-        <div>DetailBlog</div>
+        <div className="container mx-auto max-w-[1200px]">
+            <div className="pt-16">
+                <div className="container mx-auto max-w-[800px] mb-10">
+                    <div className="mb-3 font-semibold">
+                        <a href="/exercise">Exercise</a>
+                        <span className="mx-2">/</span>
+                        <span>Neck</span>
+                    </div>
+                    <div className="text-4xl font-semibold">Excercise for Neck Massage</div>
+                    <div className="mt-8 text-[#A8A29E]">
+                        <ClockCircleOutlined />
+                        <span className="ml-2 tracking-tighter">5 minutes to read</span>
+                    </div>
+
+                </div>
+                <div className="ml-32">Here is Content</div>
+                <div className="font-semibold text-center mt-5">
+                    <span>Do you think this video was helpful?</span>
+                    <div className="flex gap-3 justify-center mt-3">
+                        <img src={likeButton} />
+                        <div className="w-[49px] h-[49px] rounded-full text-center border-[2px] border-black text-2xl leading-[42px]">6</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
